6ed7a70741960b3d6311b2417c41aeec2529ccd1
idbeholdv18 <alimsadullaev18@gmail.com>
2025-04-19 13:06:24 +0300

    binary tree interface

---
+++ include/binary_tree/binary_tree.interface.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace binary_tree
+{
+    template <typename T>
+    class ITree
+    {
+    public:
+        virtual ITree<T> &add(T data) = 0;
+        virtual void dsfPreorder() = 0;
+        virtual ITree<T> &clear() = 0;
+        virtual ~ITree() = default;
+        virtual void exportToDot(std::ostream& out) = 0;
+    };
+}
